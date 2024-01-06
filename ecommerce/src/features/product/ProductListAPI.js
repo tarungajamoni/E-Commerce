b2cf3a2fa8082172b64dcf71ab1383323974a752
tarungajamoni <saitarun.goud1998@gmail.com>
2024-01-06 23:11:08 +0530

    Pagination Added

--- ecommerce/src/features/product/ProductListAPI.js
+++ ecommerce/src/features/product/ProductListAPI.js
@@ -8,18 +8,33 @@ export function fetchAllProducts() {
   );
 }
 
-export function fetchProductsByFilters(filter) {
- //filter = {'category':'smartphone'}
+export function fetchProductsByFilters(filter, sort, pagination) {
+ //filter = {'category':['smartphone','laptops']}
+ //sort = {_sort:'price',_order='desc'}
+ //pagination = _page=1,limit=10
+
  let queryString = ''; 
  for(let key in filter) {
-queryString +=`${key}=${filter[key]}&`
+  const categoryValues = filter[key]
+  if(categoryValues.length){
+  const lastCategoryValue = categoryValues[categoryValues.length - 1]
+queryString +=`${key}=${lastCategoryValue}&`
  }
+}
+for(let key in sort) {
+  queryString +=`${key}=${sort[key]}&`
+}
 
+for(let key in pagination) {
+  queryString +=`${key}=${pagination[key]}&`
+}
 
   return new Promise(async (resolve) => {
+    //no hardcoding server URL
     const response = await fetch('http://localhost:8080/products?'+queryString);
     const data = await response.json();
-    resolve({ data })
+    const totalItems = await response.headers.get('X-Total-Count')
+    resolve({ data:{products:data, totalItems:+totalItems} })
   }
   );
 }
